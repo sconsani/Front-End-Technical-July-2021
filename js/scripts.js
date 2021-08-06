93e3a4bfb355ff32f6492126437531cab6c55594
Sammy Consani <sammyconsani@Sammys-MacBook-Pro.local>
2021-08-06 12:38:54 -0700

    loaded WSJ image

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,18 @@
 
 $(document).ready(function() {
     console.log('Document ready')
+
+    // let test = document.querySelectorAll(".register-form");
+    // console.log(test);
+
+    
+    function showImage(){
+        let imageTag = document.createElement('img');
+        imageTag.src = "./assets_from_client/assets/WSJ_Horizontal.png"
+        let form = document.querySelector(".register-form");
+
+        form.prepend(imageTag);
+    };
+
+    showImage();
 });
-  
