6e43d7bddd2bc9011577591213393cb4fe1b7ddf
Sammy Consani <sammyconsani@Sammys-MacBook-Pro.local>
2021-08-09 12:17:25 -0700

    mutation observer for race condition caused by how vue renders templates

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,30 @@
 $(document).ready(function() {
     console.log('Document ready')
 
+    const targetNode = document.getElementById('app');
+    const config = { childList: true};
+    const callback = function(mutationsList, observer) {
+        for(const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                if(mutation.addedNodes.length >0) {
+                    const sform = document.querySelector(".signin-form");
+                    if (sform && sform.children.length > 0 && sform.children[0].tagName != "IMG"){
+                        let imageTagTwo = document.createElement ('img');
+                        imageTagTwo.src = "./assets_from_client/assets/WSJ_Horizontal.png";
+                        sform.prepend(imageTagTwo);
+
+                        let triangle = document.createElement ('div');
+                        triangle.setAttribute('id', 'triangle');
+                        sform.append(triangle);
+                    }
+                }
+            }
+        }
+    };
+
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+
 
 
     
