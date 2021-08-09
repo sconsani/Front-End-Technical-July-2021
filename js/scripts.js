7eda84048184a844892e7f9f87bb8a5ce8509fce
Sammy Consani <sammyconsani@sammys-mbp.lan>
2021-08-08 17:35:02 -0700

    updated js-duplicate add bug

--- js/scripts.js
+++ js/scripts.js
@@ -18,37 +18,65 @@ $(document).ready(function() {
         rform.append(triangle);
     };
 
-    function signIn(){
+
+    function addId(){
         let button = document.querySelector("button");
-        let header = document.querySelector("h1");
-
         button.addEventListener("click", function (){
-            if(button.innerText = "Sign In"){
-                header.innerText = "Sign In";
-            };
-
-         });
-
-    };
-
-    function showImageTwo(){
         let signInButton = document.querySelector("button"); 
-        signInButton.setAttribute('id', 'signin');
-        // console.log(signInButton);
-        
-        while(signInButton.classList.contains("active")){
+            signInButton.setAttribute('id', 'signin');
+            console.log(signInButton);
 
+        let sform = document.getElementsByClassName(".signin-form");
+        if(signInButton.classList.contains("active")){
             let imageTagTwo = document.createElement('img');
             imageTagTwo.src = "./assets_from_client/assets/WSJ_Horizontal.png";
             let sform = document.querySelector(".signin-form");
-            // console.log(sform);
-
-        sform.prepend(imageTagTwo);
-
+            sform.prepend(imageTagTwo);
         };
-    }
+
+        });
+    };
+
+
+    // function signIn(){
+    //     let sform = document.getElementsByClassName(".signin-form");
+    //     console.log(sform);
+    //     if()
+        // if(sform.classList.contains("active")){
+        // console.log("true");
+//             let imageTagTwo = document.createElement('img');
+//             imageTagTwo.src = "./assets_from_client/assets/WSJ_Horizontal.png";
+//             
+//             sform.prepend(imageTagTwo);
+    // };
+
+
+
+    //     let header = document.querySelector("h1");
+
+    //     button.addEventListener("click", function (){
+    //         // if(button.innerText = "Sign In"){
+    //         //     header.innerText = "Sign In"
+    //         // };
+    //         let signInButton = document.querySelector("button"); 
+    //         signInButton.setAttribute('id', 'signin');
+
+    //         if(signInButton.classList.contains("active")){
+    //             // console.log("true");
+    //             let imageTagTwo = document.createElement('img');
+    //             imageTagTwo.src = "./assets_from_client/assets/WSJ_Horizontal.png";
+    //             let sform = document.querySelector(".signin-form");
+    //             sform.prepend(imageTagTwo);
+
+
+
+    //         };
+    //     });
+    // };
 
     showImage();
-    signIn();
-    showImageTwo();
+    addId();
+    // signIn();
+    
+    // showImageTwo();
 });
